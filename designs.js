cc73dc16bb35adc006d580c92be89ec221a9ba9e
George Alcantara <georgelalcantara@gmail.com>
2017-12-04 17:16:10 -0500

    ready for resubmit

--- designs.js
+++ designs.js
@@ -1,10 +1,7 @@
 
 function makeGrid() {
 
-  var color;
-  var rows;
-  var cols;
-  var table = document.getElementById("pixel_canvas");
+  let table = document.getElementById("pixel_canvas");
 
   table.innerHTML = "";
   while(table.rows.length > 0)
@@ -26,22 +23,12 @@ function makeGrid() {
   return false;
 }
 
-function submition() {
-  document.getElementById("sizePicker").addEventListener("submit", function(event) {
+document.getElementById("sizePicker").addEventListener("submit", function(event) {
   event.preventDefault();
   makeGrid();
 });
-};
-submition();
 
-/*
-I created the Button to clear the grid. Since when I click on submit it acctually clears the grid,
-I made it a function <submition()> so I could call the function inside the function bellow.
-However, if I "uncomment" the code bellow the whole thing does not work. Can you help me with that? Thank you.
-*/
 
-// $("#clearButton").click(function(){
-//   let r = confirm("Are you sure you want to clear?");
-//   if (r == true) {
-//     submition();
-// });
+$("#clearButton").click(function(){
+    makeGrid();
+  });
